1dbc296db503a66bcdd526e7fd53a6b9d5cf4c24
RR <roznoshchik@gmail.com>
2024-05-03 19:20:54 -0400

    Types moved to global.d.ts file

---
+++ client/static/global.d.ts
@@ -0,0 +1,30 @@
+declare global {
+  type ApplicationStage =
+    "Waiting to apply" |
+    "Applied" |
+    "Interview scheduled" |
+    "Stage one - intro call" |
+    "Stage two - technical or team" |
+    "Stage three - cultural fit" |
+    "Waiting for offer" |
+    "Rejected" |
+    "Received offer" |
+    "Offer declined";
+
+
+  type Application = {
+    name: string;
+    appliedDate: Number;
+    lastAction: Number;
+    stage: ApplicationStage;
+    answered: boolean;
+    rejected: boolean;
+    pointsOfContact: string;
+    notes: string;
+    url: string;
+    usedCoverLetter: boolean;
+    coverLetterName: string;
+  };
+}
+
+export {}
